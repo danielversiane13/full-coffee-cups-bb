248414ae8ac71c42bb20ef9954c5e0d5842b9925
Daniel Versiane <daniel.versiane@gmail.com>
2021-11-01 17:06:28 -0300

    feat: add entity Customer.

---
+++ src/main/java/br/com/gamabank/bluebank/entities/Customer.java
@@ -0,0 +1,69 @@
+package br.com.gamabank.bluebank.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Customer extends SuperEntity {
+
+	private String name;
+	private String cpfCnpj;
+	private LocalDate birthDate;
+	private String email;
+	private String phone;
+
+	public Customer() {
+		super();
+	}
+
+	public Customer(String name, String cpfCnpj, LocalDate birthDate, String email, String phone) {
+		super();
+		this.name = name;
+		this.cpfCnpj = cpfCnpj;
+		this.birthDate = birthDate;
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+
+	public void setCpfCnpj(String cpfCnpj) {
+		this.cpfCnpj = cpfCnpj;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}

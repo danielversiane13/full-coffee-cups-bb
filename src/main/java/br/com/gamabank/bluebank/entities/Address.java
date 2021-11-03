caf892420cbf10856c36d53c1c2f53464ebf2a7e
eduardotecnologo <eduardotecnologo@hotmail.com>
2021-11-03 16:32:01 -0700

    Correct branch

--- src/main/java/br/com/gamabank/bluebank/entities/Address.java
+++ src/main/java/br/com/gamabank/bluebank/entities/Address.java
@@ -9,13 +9,16 @@ public class Address extends SuperEntity{
     public String state;
     public String country;
     public String zipcode;
+    public String number;
+    public String complement;
+    public String neighborhood;
     
     public Address() 
     { 
     	super(); 
     }
     
-    public Address(String street, String city, String state, String country, String zipcode)
+    public Address(String street, String city, String state, String country, String zipcode, String number, String complement, String neighborhood )
     {
     	super();
         this.street = street;
@@ -63,5 +66,29 @@ public class Address extends SuperEntity{
 
 	public void setZipcode(String zipcode) {
 		this.zipcode = zipcode;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getComplement() {
+		return complement;
+	}
+
+	public void setComplement(String complement) {
+		this.complement = complement;
+	}
+
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
 	}
 }

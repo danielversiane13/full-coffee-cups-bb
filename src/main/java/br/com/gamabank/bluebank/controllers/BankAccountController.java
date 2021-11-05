43106053038d8ac4f02076e3149686a0dc060c97
Antonio Moura <antonio.gmoura@gmail.com>
2021-11-05 19:57:30 -0400

    Update BankAccountController.java

--- src/main/java/br/com/gamabank/bluebank/controllers/BankAccountController.java
+++ src/main/java/br/com/gamabank/bluebank/controllers/BankAccountController.java
@@ -10,15 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.gamabank.bluebank.dto.BankAccountDto;
-import br.com.gamabank.bluebank.dto.CustomerDto;
 import br.com.gamabank.bluebank.exceptions.ExceptionHandler;
 import br.com.gamabank.bluebank.forms.BankAccountForm;
-import br.com.gamabank.bluebank.forms.CustomerForm;
 import br.com.gamabank.bluebank.services.BankAccountService;
 
 @RestController
@@ -44,7 +41,5 @@ public class BankAccountController {
 		URI uri = uriBuilder.path("/customers/{customerId}/bank-accounts").buildAndExpand(dto.id).toUri();
 		return ResponseEntity.created(uri).body(dto);
 	}
-	
-	
 		
 }
